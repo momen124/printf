6db7388901c953143a74737ffc8dfc786d4de06b
momen124 <momenhossny2004@gmail.com>
2023-11-11 16:02:12 +0200

    print

--- function0.c
+++ function0.c
@@ -1,27 +1,35 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+/**
+ * _printf - Produces output according to a format.
+ * @format: The format string.
+ *
+ * Return: The number of characters printed (excluding the null byte used
+ * to end output to strings).
+ */
 int _printf(const char *format, ...) {
     int count = 0;
     va_list args;
     va_start(args, format);
-int i , j ;
-    for (int i = 0; format[i] != '\0'; i++) {
+    int i = 0, j = 0;
+
+    for (i = 0; format[i] != '\0'; i++) {
         if (format[i] != '%') {
             putchar(format[i]);
             count++;
         } else {
-            i++; 
+            i++;
             switch (format[i]) {
                 case 'c': {
-                    char c = (char) va_arg(args, int);
+                    int c = va_arg(args, int);
                     putchar(c);
                     count++;
                     break;
                 }
                 case 's': {
                     char *s = va_arg(args, char *);
-                    for (int j = 0; s[j] != '\0'; j++) {
+                    for (j = 0; s[j] != '\0'; j++) {
                         putchar(s[j]);
                         count++;
                     }
