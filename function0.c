df653faf921b0b1f9a39b9f1d423155624154b83
momen124 <momenhossny2004@gmail.com>
2023-11-11 14:10:10 +0200

    print

--- function0.c
+++ function0.c
@@ -1,80 +1,48 @@
-#include <stdio.h>
 #include <stdarg.h>
+#include <stdio.h>
 
-/**
- * _printf - Produces output according to a format.
- * @format: The format string.
- *
- * Return: The number of characters printed (excluding the null byte used
- * to end output to strings).
- */
-int _printf(const char *format, ...)
-{
+int _printf(const char *format, ...) {
+    int count = 0;
     va_list args;
     va_start(args, format);
 
-    int count = 0;
-
-    while (*format)
-    {
-        if (*format == '%')
-        {
-            format++; /* Move past '%' */
-            switch (*format)
-            {
-            case 'c':
-                /* Print a character */
-                putchar(va_arg(args, int));
-                count++;
-                break;
-            case 's':
-                /* Print a string */
-                {
-                    const char *str = va_arg(args, const char *);
-                    while (*str)
-                    {
-                        putchar(*str);
-                        count++;
-                        str++;
-                    }
+    for (int i = 0; format[i] != '\0'; i++) {
+        if (format[i] != '%') {
+            putchar(format[i]);
+            count++;
+        } else {
+            i++; // skip the '%' character
+            switch (format[i]) {
+                case 'c': {
+                    char c = (char) va_arg(args, int);
+                    putchar(c);
+                    count++;
+                    break;
                 }
-                break;
-            case '%':
-                /* Print a percent sign */
-                putchar('%');
-                count++;
-                break;
-            default:
-                /* Invalid specifier, ignore */
-                break;
+                case 's': {
+                    char *s = va_arg(args, char *);
+                    for (int j = 0; s[j] != '\0'; j++) {
+                        putchar(s[j]);
+                        count++;
+                    }
+                    break;
+                }
+                case '%': {
+                    putchar('%');
+                    count++;
+                    break;
+                }
             }
         }
-        else
-        {
-            /* Regular character, just print it */
-            putchar(*format);
-            count++;
-        }
-
-        format++; /* Move to the next character in the format string */
     }
 
     va_end(args);
-
     return count;
 }
 
-int main(void)
-{
-    /* Example usage */
-    int characters_printed = _printf("This is a character: %c\n", 'A');
-    printf("Number of characters printed: %d\n", characters_printed);
-
-    characters_printed = _printf("This is a string: %s\n", "Hello, World!");
-    printf("Number of characters printed: %d\n", characters_printed);
-
-    characters_printed = _printf("This is a percent sign: %%\n");
-    printf("Number of characters printed: %d\n", characters_printed);
-
-    return (0);
+int main() {
+    _printf("Hello, World!\n");
+    _printf("This is a %c test\n", 'c');
+    _printf("This is a %% test\n");
+    return 0;
 }
