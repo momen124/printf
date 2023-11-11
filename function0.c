121b648028497c70899aeec55e1aadc050f307a8
momen124 <momenhossny2004@gmail.com>
2023-11-11 14:16:50 +0200

    print

--- function0.c
+++ function0.c
@@ -1,48 +1,80 @@
-#include <stdarg.h>
 #include <stdio.h>
+#include <stdarg.h>
 
-int _printf(const char *format, ...) {
-    int count = 0;
+/**
+ * _printf - Produces output according to a format.
+ * @format: The format string.
+ *
+ * Return: The number of characters printed (excluding the null byte used
+ * to end output to strings).
+ */
+int _printf(const char *format, ...)
+{
     va_list args;
     va_start(args, format);
 
-    for (int i = 0; format[i] != '\0'; i++) {
-        if (format[i] != '%') {
-            putchar(format[i]);
-            count++;
-        } else {
-            i++; 
-            switch (format[i]) {
-                case 'c': {
-                    char c = (char) va_arg(args, int);
-                    putchar(c);
-                    count++;
-                    break;
-                }
-                case 's': {
-                    char *s = va_arg(args, char *);
-                    for (int j = 0; s[j] != '\0'; j++) {
-                        putchar(s[j]);
+    int count = 0;
+
+    while (*format)
+    {
+        if (*format == '%')
+        {
+            format++; 
+            switch (*format)
+            {
+            case 'c':
+               
+                putchar(va_arg(args, int));
+                count++;
+                break;
+            case 's':
+               
+                {
+                    const char *str = va_arg(args, const char *);
+                    while (*str)
+                    {
+                        putchar(*str);
                         count++;
+                        str++;
                     }
-                    break;
                 }
-                case '%': {
-                    putchar('%');
-                    count++;
-                    break;
-                }
+                break;
+            case '%':
+                
+                putchar('%');
+                count++;
+                break;
+            default:
+                
+                break;
             }
         }
+        else
+        {
+            
+            putchar(*format);
+            count++;
+        }
+
+        format++; 
     }
 
     va_end(args);
+
     return count;
 }
 
-int main() {
-    _printf("Hello, World!\n");
-    _printf("This is a %c test\n", 'c');
-    _printf("This is a %% test\n");
-    return 0;
+int main(void)
+{
+    
+    int characters_printed = _printf("This is a character: %c\n", 'A');
+    printf("Number of characters printed: %d\n", characters_printed);
+
+    characters_printed = _printf("This is a string: %s\n", "Hello, World!");
+    printf("Number of characters printed: %d\n", characters_printed);
+
+    characters_printed = _printf("This is a percent sign: %%\n");
+    printf("Number of characters printed: %d\n", characters_printed);
+
+    return (0);
 }
