28020abb9e9feeed31a37dd2c684d54b44f75da8
momen124 <momenhossny2004@gmail.com>
2023-11-12 22:54:05 +0200

    print

--- function0.c
+++ function0.c
@@ -1,90 +1,67 @@
 #include "main.h"
 
-void cleanup(va_list args, buffer_t *output);
-int run_printf(const char *format, va_list args, buffer_t *output);
-int _printf(const char *format, ...);
-
 /**
- * cleanup - Peforms cleanup operations for _printf.
- * @args: A va_list of arguments provided to _printf.
- * @output: A buffer_t struct.
- */
-void cleanup(va_list args, buffer_t *output)
-{
-	va_end(args);
-	write(1, output->start, output->len);
-	free_buffer(output);
-}
-
-/**
- * run_printf - Reads through the format string for _printf.
- * @format: Character string to print - may contain directives.
- * @output: A buffer_t struct containing a buffer.
- * @args: A va_list of arguments.
+ * _printf - Produces output according to a format.
+ * @format: The format string.
  *
- * Return: The number of characters stored to output.
- */
-int run_printf(const char *format, va_list args, buffer_t *output)
-{
-	int i, wid, prec, ret = 0;
-	char tmp;
-	unsigned char flags, len;
-	unsigned int (*f)(va_list, buffer_t *,
-			unsigned char, int, int, unsigned char);
-
-	for (i = 0; *(format + i); i++)
-	{
-		len = 0;
-		if (*(format + i) == '%')
-		{
-			tmp = 0;
-			flags = handle_flags(format + i + 1, &tmp);
-			wid = handle_width(args, format + i + tmp + 1, &tmp);
-			prec = handle_precision(args, format + i + tmp + 1,
-					&tmp);
-			len = handle_length(format + i + tmp + 1, &tmp);
-
-			f = handle_specifiers(format + i + tmp + 1);
-			if (f != NULL)
-			{
-				i += tmp + 1;
-				ret += f(args, output, flags, wid, prec, len);
-				continue;
-			}
-			else if (*(format + i + tmp + 1) == '\0')
-			{
-				ret = -1;
-				break;
-			}
-		}
-		ret += _memcpy(output, (format + i), 1);
-		i += (len != 0) ? 1 : 0;
-	}
-	cleanup(args, output);
-	return (ret);
-}
-
-/**
- * _printf - Outputs a formatted string.
- * @format: Character string to print - may contain directives.
- *
- * Return: The number of characters printed.
+ * Return: The number of characters printed (excluding the null byte used
+ * to end output to strings).
  */
 int _printf(const char *format, ...)
 {
-	buffer_t *output;
 	va_list args;
-	int ret;
+	int count = 0;
 
 	if (format == NULL)
 		return (-1);
-	output = init_buffer();
-	if (output == NULL)
-		return (-1);
 
 	va_start(args, format);
+	count = parse_format(format, args);
+	va_end(args);
 
-	ret = run_printf(format, args, output);
+	return (count);
+}
 
-	return (ret);
+/**
+ * parse_format - Parses the format string and prints corresponding values.
+ * @format: The format string.
+ * @args: The va_list of arguments.
+ *
+ * Return: The number of characters printed (excluding the null byte used
+ * to end output to strings).
+ */
+int parse_format(const char *format, va_list args)
+{
+	int count = 0;
+	flags_t flags = {0, 0, 0}; /* Initialize flags to zero */
+	int (*f)(va_list, flags_t *);
+	char specifier;
+
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == '\0')
+				return (-1); /* Incomplete format specifier */
+
+			while (get_flag(*format, &flags))
+				format++;
+
+			specifier = *format;
+			f = get_print(specifier);
+
+			if (f != NULL)
+				count += f(args, &flags);
+			else
+				count += _putchar('%') + _putchar(specifier);
+		}
+		else
+		{
+			count += _putchar(*format);
+		}
+		format++;
+	}
+
+	return (count);
 }
