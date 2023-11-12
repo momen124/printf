3773ac92b418ffa1b60292a5d80f24a73cc8a2b1
MuhabGamal <muhabme@gmail.com>
2023-11-12 23:11:49 +0200

    printf

--- _printf.c
+++ _printf.c
@@ -1,9 +1,27 @@
 #include "main.h"
+#include <stdlib.h>
+
+/**
+ * itoa - Converts an integer to a string
+ * @val: Integer to convert
+ * @base: Number base for conversion
+ *
+ * Return: Pointer to the converted string
+ */
+char *itoa(int val, int base){
+    static char buf[32] = {0};
+    int i = 30;
+
+    for(; val && i ; --i, val /= base)
+        buf[i] = "0123456789abcdef"[val % base];
+
+    return &buf[i+1];
+}
 
 /**
  * _printf - Custom printf function to format and print data
  * @format: format string containing the directives
- *
+ * 
  * Return: Number of characters printed
  */
 int _printf(const char *format, ...)
@@ -12,38 +30,43 @@ int _printf(const char *format, ...)
     int count = 0;
     char ch;
     char *str;
+    int val;
 
     va_start(args, format);
 
-    while (*format)
-    {
-        if (*format == '%')
-        {
+    while (*format) {
+        if (*format == '%') {
             format++;
-            switch (*format)
-            {
-            case 'c':
-                ch = (char)va_arg(args, int);
-                putchar(ch);
-                count++;
-                break;
-            case 's':
-                str = va_arg(args, char *);
-                while (*str)
-                {
-                    putchar(*str);
-                    str++;
+            switch (*format) {
+                case 'c':
+                    ch = (char)va_arg(args, int);
+                    putchar(ch);
                     count++;
-                }
-                break;
-            case '%':
-                putchar('%');
-                count++;
-                break;
+                    break;
+                case 's':
+                    str = va_arg(args, char *);
+                    while (*str) {
+                        putchar(*str);
+                        str++;
+                        count++;
+                    }
+                    break;
+                case 'd':
+                case 'i':
+                    val = va_arg(args, int);
+                    str = itoa(val, 10);
+                    while (*str) {
+                        putchar(*str);
+                        str++;
+                        count++;
+                    }
+                    break;
+                case '%':
+                    putchar('%');
+                    count++;
+                    break;
             }
-        }
-        else
-        {
+        } else {
             putchar(*format);
             count++;
         }
--- main.c
+++ main.c
@@ -5,6 +5,8 @@ int main(void)
     _printf("Character: %c\n", 'A');
     _printf("String: %s\n", "Hello, World!");
     _printf("Percent sign: %%\n");
+    _printf("Decimal: %d\n", 123);
+    _printf("Integer: %i\n", -456);
     
     return 0;
 }
